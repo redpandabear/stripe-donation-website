1eaa26a6ba342d05ed32ae71a6c2c569f984a17b
Daniel Bruce <danieljbruce@gmail.com>
2017-07-17 14:24:21 -0400

    Changed structure to make switch to https easy

--- server.js
+++ server.js
@@ -12,13 +12,24 @@ const path = require("path");
 const react = require("react");
 const reactDOMServer = require("react-dom/server");
 const myComponent = require('./client/components/landing_page_fiat.jsx');
+const http = require('http');
+const https = require('https');
+const port = process.env.PORT || 4567; // Set the port number (dependent on whether code is in development or production)
 
+var host = "";
+if (port === 4567){ // Set the host to the proper local (might not be used yet, but useful information)
+    host = "https://localhost:"; // development
+} else {
+    host = "https://lit-journey-78750.herokuapp.com"; // production
+}
+
+// TODO: remove client folder
 app.use(require("body-parser").urlencoded({extended: false}));
 //app.use(path.join(__dirname + '/client'));
-app.set("view engine", "pug");
-app.use(express.static('public'));
+app.set("view engine", "pug"); // Setting the view engine to Pug
+app.use(express.static('public')); // Allowing rendered pages to access resources in public directory
 
-currencyDict = {
+currencyDict = { // A mapping from the symbol used for stripe to the symbol the user sees
     "cad": "CAD",
     "eur": "EUR",
     "gbp": "GBP",
@@ -27,9 +38,9 @@ currencyDict = {
 
 app.get("/", function(req, res) {
         if (process.env.PORT) {// check to see if we are working in production
-            res.render("index", {environment: "heroku"});
+            res.render("index", {environment: "heroku"}); // Render the main donation page using pug
         } else {
-            res.render("index", {environment: "development"});
+            res.render("index", {environment: "development"}); // Render the main donation page using pug
         }
     }
     //res.sendFile(path.join(__dirname, 'client', 'index.html'))
@@ -51,30 +62,36 @@ app.post("/charge", (req, res) => {
   const amount = req.body.amount * 100;
   const selectedCurrency = req.body.currency;
   const currencyString = currencyDict[selectedCurrency];
-  
+
+  // This code runs after the user enters their information into the form
   stripe.customers.create({
-    email: req.body.stripeEmail,
+    email: req.body.stripeEmail, // We create a customer based on the email and token passed by the stripe form
     card: req.body.stripeToken
   })
   .then(customer =>
-    stripe.charges.create({
-      amount: amount,
+    stripe.charges.create({ // // A charge is created based on the data in the html form's body
+      amount: amount, // 'amount' set earlier based on the body content of the charge form
       description: "Sample Charge",
-      currency: selectedCurrency,
+      currency: selectedCurrency, // 'currency' set earlier from the request body
       customer: customer.id
     }))
   .catch(err => console.log("Error:", err))
   .then(function(charge) {
-    //var props = {"amount": (charge.amount / 100).toFixed(2), "currency": currencyString};
-    //var html = reactDOMServer.renderToString(
-    //    react.createElement(myComponent, props)
-    //);
-    //res.send(html);
-    res.render("landing_page_fiat", {
-        amount:(charge.amount / 100).toFixed(2),
-        currency: currencyString
+    res.render("landing_page_fiat", { // Here we are using pug to render the output (see views folder)
+        amount:(charge.amount / 100).toFixed(2), // Here we replace 'amount' in the pug page with the user input
+        currency: currencyString // Here we replace the 'currency' on the pug page with the user input
     })
   });
 });
 
-app.listen(process.env.PORT || 4567);
+// TODO: When we get the certificate then we will replace http with https
+// TODO: Use https://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+const server = http.createServer(app).listen(port, function(err){ // This starts the server
+    if (err) {
+        console.log(err); // Print an error if the server fails to listen
+    } else {
+        const host = server.address().address;
+        const port = server.address().port;
+        console.log(`Server listening on ${host}:${port}`); // If this line prints then we can assume we are recieving requests.
+    }
+});
