d5e203d43e0469b2fd0b621a6e040daaaecab59e
Daniel Bruce <danieljbruce@gmail.com>
2017-08-01 20:11:32 -0400

    Moved stripe button outside form

--- server/route-stripe-charge.js
+++ server/route-stripe-charge.js
@@ -12,16 +12,17 @@ app.post("/charge", (req, res) => {
     // The form ends up being an iFrame that pops up
     // TODO: Follow the instructions given in https://www.youtube.com/watch?v=k66bOHX8MnY to get a React landing page
     // TODO: Make sure that req.body actually contains data from the checkout box
-    var self = this;
-    const amount = parseInt(req.body.amount * 100);
-    const selectedCurrency = String(req.body.currency);
-    const currencyString = currencyDict[selectedCurrency];
+    if (typeof(req.body.currency) !== 'undefined' && req.body.currency !== '') {
+        var self = this;
+        const amount = parseInt(req.body.amount * 100);
+        const selectedCurrency = String(req.body.currency);
+        const currencyString = currencyDict[selectedCurrency];
 
-    // This code runs after the user enters their information into the form
-    stripe.customers.create({
-        email: String(req.body.stripeEmail), // We create a customer based on the email and token passed by the stripe form
-        card: String(req.body.stripeToken)
-    })
+        // This code runs after the user enters their information into the form
+        stripe.customers.create({
+            email: String(req.body.stripeEmail), // We create a customer based on the email and token passed by the stripe form
+            card: String(req.body.stripeToken)
+        })
         .then(customer =>
             stripe.charges.create({ // // A charge is created based on the data in the html form's body
                 amount: amount, // 'amount' set earlier based on the body content of the charge form
@@ -30,10 +31,14 @@ app.post("/charge", (req, res) => {
                 customer: customer.id
             }))
         .catch(err => console.log("Error:", err))
-        .then(function(charge) {
+        .then(function (charge) {
             res.render("landing_page_fiat", { // Here we are using pug to render the output (see views folder)
-                amount:(charge.amount / 100).toFixed(2), // Here we replace 'amount' in the pug page with the user input
+                amount: (charge.amount / 100).toFixed(2), // Here we replace 'amount' in the pug page with the user input
                 currency: currencyString // Here we replace the 'currency' on the pug page with the user input
             })
         });
+    } else {
+        res.end();
+        // This code shouldn't run.
+    }
 });
