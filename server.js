927a6173c7b063351436c4d48c327f9be4469fcf
Daniel Bruce <danieljbruce@gmail.com>
2017-08-04 13:37:46 -0400

    https server preparation

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const http = require('http');
 const https = require('https');
 const port = require('./server/port-setup.js');
 const app = require('./server/app-setup.js');
+const fs = require('fs');
 
 require('./server/route-environment-development-production.js');
 require('./server/route-public-data-key.js');
@@ -20,6 +21,7 @@ require('./server/route-thank-you-fiat.js');
 
 // TODO: When we get the certificate then we will replace http with https
 // TODO: Use https://stackoverflow.com/questions/7450940/automatic-https-connection-redirect-with-node-js-express
+
 const server = http.createServer(app).listen(port, function(err){ // This starts the server
     if (err) {
         console.log(err); // Print an error if the server fails to listen
@@ -29,3 +31,30 @@ const server = http.createServer(app).listen(port, function(err){ // This starts
         console.log(`Server listening on ${host}:${port}`); // If this line prints then we can assume we are recieving requests.
     }
 });
+
+/*
+// https code for setting up a secure server
+const options = {
+    key: fs.readFileSync('https/agent-key.pem'),
+    cert: fs.readFileSync('https/agent-cert.pem')
+};
+
+const serverHttps = https.createServer(options, app).listen(port, function(err){ // This starts the server
+    if (err) {
+        console.log(err); // Print an error if the server fails to listen
+    } else {
+        const host = serverHttps.address().address;
+        const port = serverHttps.address().port;
+        console.log(`Server listening on ${host}:${port}`); // If this line prints then we can assume we are recieving requests.
+    }
+});
+
+// set up plain http server
+var http = express.createServer();
+// set up a route to redirect http to https
+http.get('*',function(req,res){
+    res.redirect('https://collider-x.org'+req.url)
+});
+// have it listen on 8080
+http.listen(8080);
+*/
